90bb96891155ccb02f89ae8dd842ff74e37b6c74
pnetoinfo07 <31972103+netiim@users.noreply.github.com>
2024-08-21 13:16:35 -0300

    criando produto repository

---
+++ Sqlite/ProdutoRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sqlite;
+
+public class ProdutoRepository
+{
+    private readonly string ConnectionString = "Data Source=LojaBD.db";
+    public void AdicionarProduto(string nome, double preco)
+    {
+        using (var connection = new SQLiteConnection(ConnectionString))
+        {
+            connection.Open();
+            string comandInsert = @"INSERT INTO Produtos(Nome,Preco) 
+                                    VALUES (@Nome,@Preco)";
+
+            using (var command = new SQLiteCommand(comandInsert, connection))
+            {
+                command.Parameters.AddWithValue("@Nome", nome);
+                command.Parameters.AddWithValue("@Preco", preco);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
--- Sqlite/Program.cs
+++ Sqlite/Program.cs
@@ -1,3 +1,29 @@
 ﻿using Sqlite;
 
 InicializadorBd.Inicializar();
+ProdutoRepository _repository = new ProdutoRepository();
+int opcao = -1;
+
+while (opcao != 0)
+{
+    Console.WriteLine("====     MENU DE PRODUTOS    ===");
+    Console.WriteLine("1 - Adiconar Produto");
+    Console.WriteLine("0 - Sair");
+
+    opcao = int.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1:
+            Console.WriteLine("Nome do Produto");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Preço do Produto");
+            double preco = double.Parse(Console.ReadLine());
+            _repository.AdicionarProduto(nome, preco);
+            Console.WriteLine("Produto adicionado com sucesso.");
+            break;
+        case 0:
+            Console.WriteLine("Saindo . . . ");
+            break;
+    }
+}
