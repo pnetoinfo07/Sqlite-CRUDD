e28458e0c65ebaf242c76feffb06db0df1156ec9
pnetoinfo07 <31972103+netiim@users.noreply.github.com>
2024-08-22 13:12:16 -0300

    Versao Final

--- Sqlite/ProdutoRepository.cs
+++ Sqlite/ProdutoRepository.cs
@@ -32,9 +32,58 @@ public class ProdutoRepository
         using (var connection = new SQLiteConnection(ConnectionString))
         {
             connection.Open();
-            var selectCommand = "SELECT Id, Nome, Preco FROM Produtos;";
+            string selectCommand = "SELECT Id, Nome, Preco FROM Produtos;";
             using (var command = new SQLiteCommand(selectCommand, connection))
             {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Id: {reader["Id"]} - Nome: {reader["Nome"]} - Preco: {reader["Preco"]}");
+                    }
+                }
+            }
+        }
+    }
+    public void EditarProduto(int id, string nome, double preco)
+    {
+        using (var connection = new SQLiteConnection(ConnectionString))
+        {
+            connection.Open();
+            string updateCommand = @"UPDATE Produtos
+                                  SET Nome = @Nome, Preco = @Preco
+                                  WHERE Id = @Id";
+            using (var command = new SQLiteCommand(updateCommand, connection))
+            {
+                command.Parameters.AddWithValue("@Id", id);
+                command.Parameters.AddWithValue("@Nome", nome);
+                command.Parameters.AddWithValue("@Preco", preco);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+    public void ExcluirProduto(int id)
+    {
+        using (var connection = new SQLiteConnection(ConnectionString))
+        {
+            connection.Open();
+            string deleteCommand = "DELETE FROM Produtos WHERE Id = @Id";
+            using (var command = new SQLiteCommand(deleteCommand, connection))
+            {
+                command.Parameters.AddWithValue("@Id", id);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+    public void BuscarProdutoPorId(int id)
+    {
+        using (var connection = new SQLiteConnection(ConnectionString))
+        {
+            connection.Open();
+            string selectCommand = "SELECT Id, Nome, Preco FROM Produtos WHERE Id = @Id;";
+            using (var command = new SQLiteCommand(selectCommand, connection))
+            {
+                command.Parameters.AddWithValue("@Id", id);
                 using (var reader = command.ExecuteReader())
                 {
                     while (reader.Read())
--- Sqlite/Program.cs
+++ Sqlite/Program.cs
@@ -9,6 +9,9 @@ while (opcao != 0)
     Console.WriteLine("====     MENU DE PRODUTOS    ===");
     Console.WriteLine("1 - Adiconar Produto");
     Console.WriteLine("2 - Listar Produtos");
+    Console.WriteLine("3 - Editar Produto");
+    Console.WriteLine("4 - Excluir Produto");
+    Console.WriteLine("5 - Buscar Produto por Id");
     Console.WriteLine("0 - Sair");
 
     opcao = int.Parse(Console.ReadLine());
@@ -25,6 +28,25 @@ while (opcao != 0)
             break;
         case 2:
             _repository.ListarProdutos();
+            break;
+        case 3:
+            Console.WriteLine("Qual Id do produto que deseja editar");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nome do Produto");
+            nome = Console.ReadLine();
+            Console.WriteLine("Preço do Produto");
+            preco = double.Parse(Console.ReadLine());
+            _repository.EditarProduto(id,nome,preco);
+            break;
+        case 4:
+            Console.WriteLine("Qual Id do produto que deseja editar");
+            id = int.Parse(Console.ReadLine());
+            _repository.ExcluirProduto(id);
+            break;
+        case 5:
+            Console.WriteLine("Qual Id do produto que deseja editar");
+            id = int.Parse(Console.ReadLine());
+            _repository.BuscarProdutoPorId(id);
             break;
         case 0:
             Console.WriteLine("Saindo . . . ");
